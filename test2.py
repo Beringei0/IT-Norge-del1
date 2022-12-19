a032ecb9fb00d2a3ca570963c90fb078335be095
Beringei0 <96120896+Beringei0@users.noreply.github.com>
2022-12-19 11:24:20 +0100

    Delete test2.py

--- test2.py
+++
@@ -1,7 +0,0 @@
-import wmi
-w = wmi.WMI()
-for p in w.Win32_Product():
-    print( r"\newcommand*{\Title}", "{" + p.Version +"}")
-    print( r"\newcommand*{\Title}", "{" + p.Vendor +"}")
-    print( r"\newcommand*{\Title}", "{" + p.Caption +"}")
-    print("\hline")
